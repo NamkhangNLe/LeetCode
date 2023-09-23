f79287076bcdeb0748b75ca885ce08a049cf5e04
Namkhang Le <LENN@gatech.edu>
2023-09-23 19:11:01 -0400

    Create MergeTwoSortedLists.java

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { 
+            this.val = val; 
+        }
+ *     ListNode(int val, ListNode next) {
+            this.val = val; 
+            this.next = next;
+            }
+ * }
+ */
+class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode();
+        ListNode curr = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = list1 != null ? list1 : list2;
+        return head.next;
+    } 
+}
