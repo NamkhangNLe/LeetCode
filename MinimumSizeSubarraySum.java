d5199b3e5e63889c0d4701e51965cd5758940aac
Namkhang Le <Namkhangnle@hotmail.com>
2024-10-04 12:51:24 -0400

    Update MinimumSizeSubarraySum.java

--- MinimumSizeSubarraySum.java
+++ MinimumSizeSubarraySum.java
@@ -1,13 +1,13 @@
 class MinimumSizeSubarraySum {
     public int minSubArrayLen(int target, int[] nums) {
-        int l = 0;
+        int left = 0;
         int sum = 0;
         int length = Integer.MAX_VALUE;
-        for (int r = 0; r < nums.length; r++) {
-            sum += nums[r];
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
             while (sum >= target) {
-                length = Math.min(length, r - l + 1);
-                sum -= nums[l++];
+                length = Math.min(right - left + 1, length);
+                sum -= nums[left++];
             }
         }
         return length == Integer.MAX_VALUE ? 0 : length;
